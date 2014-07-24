fda87307fabec29df96cb81a860b5d3b94068477
Garrett <gurt1@gurt.com>
2014-07-24 10:08:41 -0700

    Update LaserController.cs
    Added hunt function.

--- TowerDefender/LaserController.cs
+++ TowerDefender/LaserController.cs
@@ -11,15 +11,12 @@ namespace TowerDefender
     public class LaserController
     {
         private SerialPort _port;
-        private float _x, _currentX = 150; //make this the default x position
-        private float _y, _currentY = 100; //make this the default y position
-        private int stepsPerPixelX = 2; //adjust this to correspond to calibration of X
-        private int stepsPerPixelY = 2; //adjust this to correspond to calibration of Y
-        private bool inverseX = true;
-        private bool inverseY = false;
+        private float _x = 150; //make this the default x position
+        private float _y = 100; //make this the default y position
+
         private DateTime _lastFired = DateTime.MinValue;
         private bool _isFiring;
-        //private float gain = 0.5f;
+        private float gain = 0.5f;
         private bool _updated;
     
         public void Connect(SerialPort port)
@@ -30,7 +27,7 @@ namespace TowerDefender
 
         public void Update(float deltax, float deltay, bool shouldFire)
         {
-       /*     if (deltax > 0)
+            if (deltax > 0)
                 _x += gain;
             if (deltax < 0)
                 _x -= gain;
@@ -40,11 +37,11 @@ namespace TowerDefender
             if (deltay > 0)
                 _y -= gain;
 
-            _x = Math.Min(Math.Max(_x, 0), 700); //range of hardware
-            _y = Math.Min(Math.Max(_y, 0), 400); //range of hardware
+            _x = Math.Min(Math.Max(_x, 0), 350); //range of hardware
+            _y = Math.Min(Math.Max(_y, 0), 200); //range of hardware
 
-    */
-    /*
+    
+            /*
             if (shouldFire && !_isFiring && (DateTime.Now - _lastFired).TotalMilliseconds > 2000)
             {
                 _isFiring = true;
@@ -57,34 +54,68 @@ namespace TowerDefender
             }
             */
             
-
-
-
         if (_updated){
-            
-           _currentX = _x;
-           _currentY = _y;
-           
-           if (inverseX)
-           {
-            _x = 640 - (deltax * stepsPerPixelX);
-           } else {
-            _x = deltax * stepsPerPixelX;   
-           }
-           
-            if (inverseY)
-           {
-            _y = 480 - (deltay * stepsPerPixelY);
-           } else {
-            _y = deltay * stepsPerPixelY;   
-           }
-            
             
             _isFiring = true;
             var text = String.Format("{0:000},{1:000},{2}\r\n", _x, _y, _isFiring ? 1 : 0);
             //Thread.Sleep(100);
             _port.Write(text);
             _updated = false;
+        }
+
+            var resp = _port.ReadExisting();
+            if (resp == "updated"){ //I'm expecting a response of "updated" from the arduino
+             _updated = true;
+               
+            }
+        }
+        
+        
+        
+        
+        public void Hunt()  //Cant find any targets, so hunt for them
+        {
+            
+       gain = 2f;
+       bool panPlus = true;
+       bool tiltPlus = true;
+        
+            if ((_x - gain)  < 0) //set value to the minimum pan search value
+               panPlus = true;
+               
+            if ((_x + gain) > 350) //set value to the maximum pan search value
+                panPlus = false;
+
+            if ((_y - gain) < 0) //set value to the minimum tilt search value
+                tiltPlus = true;
+                
+            if ((_y + gain) > 100) //set value to the maximum tilt search value
+                tiltlus = false;
+              
+              
+                if (panPlus) {
+                 _x += gain;   
+                } else {
+                 _x -= gain;     
+                }
+                
+                if (tiltPlus) {
+                 _y += gain;   
+                } else {
+                 _y -= gain;     
+                }
+
+            _x = Math.Min(Math.Max(_x, 0), 700); //range of hardware
+            _y = Math.Min(Math.Max(_y, 0), 400); //range of hardware
+
+    
+
+        if (_updated){
+          
+            _isFiring = false;
+            var text = String.Format("{0:000},{1:000},{2}\r\n", _x, _y, _isFiring ? 1 : 0);
+            //Thread.Sleep(100);
+            _port.Write(text);
         }
 
             var resp = _port.ReadExisting();
